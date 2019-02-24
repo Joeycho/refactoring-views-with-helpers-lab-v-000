44d1bcb143d81ec2c2aad2f0f622e241e5ec6dda
Joey Cho <cjy12182000@naver.com>
2019-02-24 06:53:58 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,16 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def artist_name
+    self.name
+  end
+
+  def artist_name=(name)
+    if name != "" && name != nil
+      self.name = name
+      self.save
+    else
+
+    end      
+  end
 end
