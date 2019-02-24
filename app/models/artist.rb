f6ad2ab0101e1a5fd88456a031b8e4e3a3ff1620
Joey Cho <cjy12182000@naver.com>
2019-02-24 06:59:53 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,17 +1,4 @@
 require 'pry'
 class Artist < ActiveRecord::Base
   has_many :songs
-
-  def artist_name
-    self.name
-  end
-
-  def artist_name=(name)
-    if name != "" && name != nil
-      self.name = name
-      self.save
-    else
-      errors.add(:name, "The given name is not valid")
-    end
-  end
 end
