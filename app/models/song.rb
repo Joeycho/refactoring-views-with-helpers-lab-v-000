9b6199b0c3aa192e2809cf150d4303290fd587f3
Joey Cho <cjy12182000@naver.com>
2019-02-24 07:03:51 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,8 +7,12 @@ class Song < ActiveRecord::Base
 
   def artist_name=(name)
     if name != "" && name != nil
-      self.artist.name = name
-      self.artist.save
+      if self.artist != nil
+        self.artist.name = name
+        self.artist.save
+      else
+        artist = Artist.find_or_create_by(name: name)
+      end
     else
       errors.add(:artist_name, "The given name is not valid")
     end
